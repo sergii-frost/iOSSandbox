c539dd3fa6a4e1c9869f7e98505ae0b97706c5ba
Sergey Nezdoliy MBP15 <sergey.nezdoliy@gmail.com>
2013-01-12 13:51:15 +0200

    Cocos2d initial project commit.

---
+++ WordFinder/WordFinder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordFinder
+//
+//  Created by Sergey Nezdoliy on 1/12/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
